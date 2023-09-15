b244d53baf7f1067c9e6f1b0e523964f80f66a4d
Shivam Puri <puri46394@gmail.com>
2023-09-15 17:52:59 +0530

    resume updated on portfoio

--- src/components/Download.js
+++ src/components/Download.js
@@ -4,7 +4,7 @@ import '../styling/herosection.css'
 const Download = () => {
   return (
     <section className='button_section'>
-      <a href="images/Shivam's Resume.pdf" download>
+      <a href="images/shivam_puri_resume.pdf" download>
         <button className='download_button' >Download Resume
           <img src="images/download.png" height={"20px"} alt="" />
         </button>
