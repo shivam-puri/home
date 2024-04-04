e5de2f72b9fb4b44844fd597593d14fed2756643
shivm29 <puri46394@gmail.com>
2024-04-05 00:23:29 +0530

    email updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,39 +2,39 @@ import React from 'react'
 import '../styling/contact.css'
 
 const Contact = () => {
-  return (
-    <section className='contact_section' id='contactscroll' >
-        <div className="contact_heading">
-            CONTACT
-        </div>
-        <div className="contact_line">
-        Don't be Shy, Drop me a Line! &nbsp; 👇
-        </div>
+    return (
+        <section className='contact_section' id='contactscroll' >
+            <div className="contact_heading">
+                CONTACT
+            </div>
+            <div className="contact_line">
+                Don't be Shy, Drop me a Line! &nbsp; 👇
+            </div>
 
-        <div className="contact_links_container">
-            <div className="contact_link">
-                <div className="contact_icon"><i class="fa-regular fa-envelope"></i></div>
-                <div className="contact_details">
-                    <div className="contact_head">MAIL</div>
-                    <div className="contact_hyperlink">
-                        <a href="mailto:puri46394@gmail.com">puri46394@gmail.com</a>
+            <div className="contact_links_container">
+                <div className="contact_link">
+                    <div className="contact_icon"><i class="fa-regular fa-envelope"></i></div>
+                    <div className="contact_details">
+                        <div className="contact_head">MAIL</div>
+                        <div className="contact_hyperlink">
+                            <a href="mailto:shivm.onweb@gmail.com">shivm.onweb@gmail.com</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="contact_link">
-                <div className="contact_icon"><i class="fa-solid fa-location-dot"></i></div>
-                <div className="contact_details">
-                    <div className="contact_head">LOCATION</div>
-                    <div className="contact_hyperlink">
-                        <a href="">Indore,&nbsp;India</a>
+                <div className="contact_link">
+                    <div className="contact_icon"><i class="fa-solid fa-location-dot"></i></div>
+                    <div className="contact_details">
+                        <div className="contact_head">LOCATION</div>
+                        <div className="contact_hyperlink">
+                            <a href="">Indore,&nbsp;India</a>
+                        </div>
                     </div>
                 </div>
+
+
             </div>
-            
-           
-        </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default Contact
