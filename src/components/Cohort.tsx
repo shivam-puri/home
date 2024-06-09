692fdd3c87a4d67af0f79d281321013c56c29a35
shivm29 <puri46384@gmail.com>
2024-06-09 23:26:18 +0530

    image fixed

--- src/components/Cohort.tsx
+++ src/components/Cohort.tsx
@@ -6,8 +6,8 @@ const Cohort = () => {
 
             <div className="py-5 w-full " >
                 <div className="flex flex-col p-2 rounded-lg bg-hover-bg" >
-                    <img className="w-full rounded-md" src="public/batch2.jpg" alt="" />
-                    <img className="w-full mb-2 rounded-md" src="public/batch1.jpg" alt="" />
+                    <img className="w-full rounded-md" src="/batch2.jpg" alt="" />
+                    <img className="w-full mb-2 rounded-md" src="/batch1.jpg" alt="" />
                     <p className="text-xs p-2 mb-2 tracking-wide" >During my second year, conducted multiple offline cohorts,   teaching DSA and C++ to nearly 100 students, including juniors, batchmates, and some seniors.</p>
                 </div>
             </div>
