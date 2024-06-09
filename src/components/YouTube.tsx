0c6e17e13b971e4c401e9e8ea2da58f240579ab2
shivm29 <puri46384@gmail.com>
2024-06-09 23:01:03 +0530

    iframes resolved

--- src/components/YouTube.tsx
+++ src/components/YouTube.tsx
@@ -8,9 +8,7 @@ const YouTube = () => {
             </span>
             <iframe
                 width="100%" className="mb-2"
-
-                src={`https://www.youtube.com/embed/qNIW75-AuII?si=xQ_azhCMuJ_QqlJM"`}
-                frameBorder="0"
+                src={`https://www.youtube.com/embed/qNIW75-AuII?si=xQ_azhCMuJ_QqlJM`}
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
                 title="Embedded YouTube"
@@ -18,8 +16,7 @@ const YouTube = () => {
             <iframe
                 width="100%"
                 className="mb-2"
-                src={`https://www.youtube.com/embed/3weEJPDOjLQ?si=RrIjEv1GDgPGIw0y"`}
-                frameBorder="0"
+                src={`https://www.youtube.com/embed/3weEJPDOjLQ?si=RrIjEv1GDgPGIw0y`}
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
                 title="Embedded YouTube"
@@ -27,22 +24,27 @@ const YouTube = () => {
             <iframe
                 width="100%"
                 className="youtube-video"
-                src={`https://www.youtube.com/embed/qNIW75-AuII?si=xQ_azhCMuJ_QqlJM`}
+                src={`https://www.youtube.com/embed/600IBRpUbes?si=ZwGWJqGqr1gWf79C`}
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
                 title="Embedded YouTube"
             />
 
 
+
             <span className="mt-10 mb-7 text-sm" >Sometimes, I also enjoy recording videos while doing my favorite Work.
             </span>
 
-            <iframe width="100%" className="mb-2" src="https://www.youtube.com/embed/_o8ePFZZ34U?si=dTCyT54Zd_q5qYa2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="100%" className="mb-2" src={`https://www.youtube.com/embed/_o8ePFZZ34U?si=dTCyT54Zd_q5qYa2`} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Embedded YouTube"></iframe>
 
             <span className="mt-10 mb-7 text-sm" >I remember starting a JavaScript playlist on YouTube, but I never finished it, lol 😭
             </span>
 
-            <iframe width="100%" className="mb-2" src="https://www.youtube.com/embed/coI553p1Gps?si=TPK_jWF6HPXW1gVi" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="100%" className="mb-2" src="https://www.youtube.com/embed/coI553p1Gps?si=TPK_jWF6HPXW1gVi" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Embedded YouTube"></iframe>
 
             <div className="mt-16 h-20 "  >
                 <p className="text-sm" >Looks like you reached the end. <a className="text-accent underline underline-offset-2 font-semibold" href="public/Shivam_Puri_Goswami_Resume.pdf" download>Click Here</a> if you want to watch/download my resume :)</p>
